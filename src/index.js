86f7b2bcca2cde77b9350eb0e0abf5043b62a488
iammarsel <fetmarsel2002@gmail.com>
2023-05-15 14:06:54 -0400

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
